6d8f818e34c5b539187fc3df33504cc8ddc16adb
Alex Smetukh <alex.smetukh@gmail.com>
2019-07-25 20:08:11 +0300

    Initial commit. GraphQL Schema & Root Query. Migration to mLab

---
+++ server/server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('../schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 3005;
+
+mongoose.connect('mongodb://ALEX:doorbell1@ds147975.mlab.com:47975/graphql-tutorial', { useNewUrlParser: true });
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log('Connected to DB!'));
+
+app.listen(PORT, err => {
+  err ? console.log(err) : console.log('Server started!');
+});
